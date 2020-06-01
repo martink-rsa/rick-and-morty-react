a6b64da273f06b399d31d58fa08e50e54648d82f
Martin Kruger <martin@martinkruger.com>
2020-06-02 01:45:03 +0200

    Feature: Fetch data - Added Character component, save point

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Character = () => {
-  return <div>Character</div>;
+const Character = ({ data }) => {
+  // const { name, origin, species, location, status, image } = props.data;
+  console.log(data);
+  const { name, origin, species, location, status, image, gender } = data;
+  return (
+    <div>
+      <div>Name: {name}</div>
+      <div>Species: {species}</div>
+      <div>Status: {status}</div>
+      <div>
+        Origin: <a href={origin.url}>{origin.name}</a>
+      </div>
+      <div>
+        Location: <a href={location.url}>{location.name}</a>
+      </div>
+      <div>
+        <img src={image} alt={name} />
+      </div>
+    </div>
+  );
 };
 
 export default Character;
