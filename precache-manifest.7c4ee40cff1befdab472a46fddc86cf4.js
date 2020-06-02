ea92b8b23718a45ceb443074b9945ab44c463029
Martin Kruger <martin@martinkruger.com>
2020-06-02 20:01:50 +0200

    Updates

---
+++ precache-manifest.7c4ee40cff1befdab472a46fddc86cf4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681fe68a400a3c9a36448dffd4d86aa9",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "9e25ee5b736bd123f357",
+    "url": "/rick-and-morty-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a84f14852f9a9515c1ef",
+    "url": "/rick-and-morty-react/static/css/main.cdf2c84d.chunk.css"
+  },
+  {
+    "revision": "9e25ee5b736bd123f357",
+    "url": "/rick-and-morty-react/static/js/2.cda5e51a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rick-and-morty-react/static/js/2.cda5e51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a84f14852f9a9515c1ef",
+    "url": "/rick-and-morty-react/static/js/main.c3986df3.chunk.js"
+  },
+  {
+    "revision": "bb3be48fd4a2f59e643d",
+    "url": "/rick-and-morty-react/static/js/runtime-main.55e243e0.js"
+  },
+  {
+    "revision": "7f7e0249fc7ae686255cf97f3b4360a9",
+    "url": "/rick-and-morty-react/static/media/external-link.7f7e0249.svg"
+  },
+  {
+    "revision": "c4c4110c826ac67bfcb670d5279a1aff",
+    "url": "/rick-and-morty-react/static/media/rm-bg.c4c4110c.jpg"
+  }
+]);
